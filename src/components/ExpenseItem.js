6990f5c96255987e76093b0449c11916c0aed635
Tarun Singh Mahar <tsmahar1997@gmail.com>
2022-02-12 13:20:12 +0530

    added new component for date and css for that

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      {/* <ExpenseDate></ExpenseDate> or because there is no content between opening and closing tag*/} 
+      <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
