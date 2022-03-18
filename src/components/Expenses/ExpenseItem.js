912b555cbb8e30c387d0fadfbe338eb5145a8a6d
Tarun Singh Mahar <62583757+tarunrpmahar@users.noreply.github.com>
2022-03-18 21:01:07 +0530

    "useState" hook (#4)
    * "useState" hook
    
    * added expense form component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+  //using it we can update the state of any variables like title 
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
+  //------
+  //let title = props.title;
+
   //Event Listner
   // function clickHandler() {}
   const clickHandler = () => {
-      console.log('Clicked!');
+      setTitle('Updated!');
+      console.log(title);
   };
 
   return (
@@ -16,7 +25,7 @@ const ExpenseItem = (props) => {
       {/* <ExpenseDate></ExpenseDate> or because there is no content between opening and closing tag so we can use self closing tag*/} 
       <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
